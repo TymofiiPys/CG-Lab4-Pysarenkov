76ca994938049346d9d1702f415a89eca1244bd3
TymofiiPys <71066289+TymofiiPys@users.noreply.github.com>
2024-05-21 22:21:47 +0300

    some more geom tests

--- src/test/java/org/delaunay/util/GeometricTest.java
+++ src/test/java/org/delaunay/util/GeometricTest.java
@@ -19,5 +19,15 @@ public class GeometricTest extends TestCase {
         assertFalse(Geometric.inCircle(p1, p2, p3, p));
         p = new Point2D.Double(2, 0.0000001);
         assertTrue(Geometric.inCircle(p1, p2, p3, p));
+        p = new Point2D.Double(0, 4);
+        assertFalse(Geometric.inCircle(p1, p2, p3, p));
+        p = new Point2D.Double(1, 3);
+        assertTrue(Geometric.inCircle(p1, p2, p3, p));
+        p = new Point2D.Double(3, 3);
+        assertTrue(Geometric.inCircle(p1, p2, p3, p));
+        p = new Point2D.Double(4, 4);
+        assertFalse(Geometric.inCircle(p1, p2, p3, p));
+        p = new Point2D.Double(0, 0);
+        assertFalse(Geometric.inCircle(p1, p2, p3, p));
     }
 }
