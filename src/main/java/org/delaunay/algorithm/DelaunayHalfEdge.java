6c3eb3498d2b8b1fb86883fdb1b477af812aeae7
TymofiiPys <71066289+TymofiiPys@users.noreply.github.com>
2024-05-22 14:04:21 +0300

    Add descr to DelaunayHalfEdge, add javafx

--- src/main/java/org/delaunay/algorithm/DelaunayHalfEdge.java
+++ src/main/java/org/delaunay/algorithm/DelaunayHalfEdge.java
@@ -2,6 +2,12 @@ package org.delaunay.algorithm;
 
 import lombok.Data;
 
+/**
+ * Represents a half-edge, containing start, endpoint ids, id of a triangle to the left and
+ * a pointer to its twin, which has start and endpoint ids swapped and id of a triangle
+ * to the right. Better use with a hashmap where a pair of start and end ids is a key,
+ * and the half-edge is the value.
+ */
 @Data
 public class DelaunayHalfEdge {
     private final int start;
