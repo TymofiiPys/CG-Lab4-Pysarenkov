0de7e65f23a4402e5f05512f0095849dc33a22bf
TymofiiPys <71066289+TymofiiPys@users.noreply.github.com>
2024-05-22 14:06:47 +0300

    triangle, with alternative version in comms

--- src/main/java/org/delaunay/algorithm/Triangle.java
+++ src/main/java/org/delaunay/algorithm/Triangle.java
@@ -3,10 +3,20 @@ package org.delaunay.algorithm;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+
 @Getter
 @RequiredArgsConstructor
 class Triangle {
     private final int id1;
     private final int id2;
     private final int id3;
+
+    // Possible alternative, maybe pointers to edges will be stored instead of ids
+    // because hashmap is used:
+//    private final int halfEdge1;
+//    private final int halfEdge2;
+//    private final int halfEdge3;
+
+    private final ArrayList<DelaunayNotInsertedPoint> containedPoints;
 }
